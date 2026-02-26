73a50d29d5395deaff6264fc489efd6fd5c2d062
Iddo Gino <iddo.gino@gmail.com>
2026-02-26 14:12:05 -0800

    devops: added coverage to github actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'json-summary', 'json'],
       include: ['src/**/*.ts', 'src/**/*.tsx'],
       exclude: ['src/**/*.test.ts', 'src/**/*.d.ts', 'src/main/**'],
     },
